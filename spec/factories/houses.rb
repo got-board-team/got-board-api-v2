bdf7beba363e3b01b5a11143587379fcf39c6739
Lucas Mansur <lucas.mansur2@gmail.com>
2017-09-07 13:29:59 -0300

    fixup! Creates resource tracks

--- spec/factories/houses.rb
+++ spec/factories/houses.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
     association :blade_track
     association :raven_track
     association :throne_track
+    association :supply_track
+    association :victory_track
 
     name House::VALID_HOUSES.sample
     throne_position 1
