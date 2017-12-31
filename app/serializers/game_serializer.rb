b1c088527889a5183f9d7ea4f720fa7a984da82c
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2017-12-31 15:57:58 -0200

    Serializes only orders instead order kinds

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,33 +1,18 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :round, :wildling_threat
 
-  has_many :orders
   has_many :houses
-
+  has_many :units
+  has_many :orders
+  has_many :power_tokens
   has_many :house_cards
 
+  has_many :neutral_force_tokens
+  has_many :garrison_tokens
+
   has_many :influence_tokens
-  has_many :iron_throne_tokens
-  has_many :fiefdom_tokens
-  has_many :kings_court_tokens
   has_many :supply_tokens
   has_many :victory_tokens
 
-  has_many :march_orders
-  has_many :support_orders
-  has_many :raid_orders
-  has_many :defense_orders
-  has_many :consolidation_orders
-
-  has_many :footmen
-  has_many :knights
-  has_many :ships
-  has_many :siege_engines
-
-  has_many :power_tokens
-
-  has_many :neutral_force_tokens
-
-  has_many :garrison_tokens
   has_many :territories
 end
