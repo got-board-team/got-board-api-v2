e6187af93d8f36ff26c8fd6ae48efb2405fa601b
Lucas Mansur <lucas.mansur2@gmail.com>
2018-07-22 15:16:47 -0300

    Small code style fix

--- app/interactions/games/create.rb
+++ app/interactions/games/create.rb
@@ -4,17 +4,17 @@ module Games
 
     def execute
       Game.transaction do
-        game = Game.create
-        compose(CreateHouses, game: game, number_of_houses: number_of_houses)
-        compose(CreateTokens, game: game)
-        compose(CreateUnits, game: game)
-        compose(CreateOrders, game: game)
-        compose(CreatePowerTokens, game: game)
-        compose(CreateHouseCards, game: game)
-        compose(CreateNeutralForceTokens, game: game)
-        compose(CreateGarrisonTokens, game: game)
-        game.save
-        game
+        Game.create.tap do |game|
+          compose(CreateHouses, game: game, number_of_houses: number_of_houses)
+          compose(CreateTokens, game: game)
+          compose(CreateUnits, game: game)
+          compose(CreateOrders, game: game)
+          compose(CreatePowerTokens, game: game)
+          compose(CreateHouseCards, game: game)
+          compose(CreateNeutralForceTokens, game: game)
+          compose(CreateGarrisonTokens, game: game)
+          game.save
+        end
       end
     end
   end
