d56a663159bb49858f7bad1eb3b710b8c3cbd025
Lucas Mansur <lucas.mansur2@gmail.com>
2018-07-23 19:39:24 -0300

    Add wildling_cards to game serializer spec

--- spec/serializers/game_serializer_spec.rb
+++ spec/serializers/game_serializer_spec.rb
@@ -4,12 +4,20 @@ RSpec.describe GameSerializer, type: :serializer do
   let(:game) { create(:game) }
   let(:serialized_game) { serialize(game) }
 
-  it "should serialize its attributes" do
+  it "serializes its attributes" do
     expect(serialized_game["data"].keys).to match_array(%w[id attributes type relationships])
   end
 
+  context "attributes" do
+    it "serializes its attributes" do
+      expect(serialized_game["data"]["attributes"].keys).to match_array(
+        %w[round wildling-threat wildling-cards]
+      )
+    end
+  end
+
   context "relationships" do
-    it "should serialize its relationships" do
+    it "serializes its relationships" do
       expect(serialized_game["data"]["relationships"].keys).to match_array(
         %w[territories houses units orders supply-tokens victory-tokens
            power-tokens house-cards neutral-force-tokens garrison-tokens
