1e4dbad0eb92781f0cee825df20887b469ff9245
Lucas Mansur <lucas.mansur2@gmail.com>
2018-08-08 16:36:56 -0300

    Rename type to unit_type

--- app/serializers/unit_serializer.rb
+++ app/serializers/unit_serializer.rb
@@ -2,7 +2,7 @@ class UnitSerializer
   include FastJsonapi::ObjectSerializer
 
   set_key_transform :dash
-  attributes :id, :unit_type, :house_name, :territory, :x, :y
+  attributes :unit_type, :house_name, :territory, :x, :y
 
   belongs_to :game
 
