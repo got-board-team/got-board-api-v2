32e6bcafd894f89725407d389fbbf6b693c9f913
Lucas Mansur <lucas.mansur2@gmail.com>
2019-02-05 20:02:42 -0200

    Add migration for Westeros cards and decks

---
+++ db/migrate/20190205215508_create_westeros_decks.rb
@@ -0,0 +1,20 @@
+class CreateWesterosDecks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :westeros_decks do |t|
+      t.bigint :tier, null: false, index: true
+
+      t.bigint :game_id, null: false, index: true
+
+      t.timestamps
+    end
+
+    create_table :westeros_cards do |t|
+      t.string :title, null: false
+      t.string :revealed, default: false, null: false
+
+      t.bigint :westeros_deck_id, null: false, index: true
+
+      t.timestamps
+    end
+  end
+end
