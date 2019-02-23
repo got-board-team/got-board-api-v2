7c0ae9a980712f03e62b186fef5ecce65eb57bc1
Vagner Zampieri <vagner.zampieri@myfreecomm.com.br>
2019-02-23 13:38:50 -0300

    Created methods to encode and decode JWT

---
+++ lib/json_web_token.rb
@@ -0,0 +1,25 @@
+# Generate token
+# Use:
+# token = JsonWebToken.encode(user_id: 42)
+# JsonWebToken.decode(token)
+class JsonWebToken
+  class << self
+    def encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, secret_key_base, 'HS256')
+    end
+
+    def decode(token)
+      body = JWT.decode(token, secret_key_base, true, algorithm: 'HS256')[0]
+      HashWithIndifferentAccess.new body
+    rescue
+      nil
+    end
+
+    private
+
+    def secret_key_base
+      Rails.application.secrets.secret_key_base
+    end
+  end
+end
---
+++ spec/lib/json_web_token_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+require_dependency 'json_web_token'
+
+RSpec.describe JsonWebToken, type: :lib do
+  let(:token_str) do
+    'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0MiwiZXhwIjoxNDg4MTY0NDAwfQ.ZMHeNY9QTe0B3VLkmiIGxjXayJGWSRnYc1tTqcqThLI'
+  end
+
+  describe '.encode' do
+    it 'generates token' do
+      Timecop.freeze(Time.local(2017, 2, 26)) do
+        token = described_class.encode(user_id: 42)
+        expect(token).to eq(token_str)
+      end
+    end
+
+    it 'generates other token' do
+      Timecop.freeze(Time.local(2017, 2, 27)) do
+        token = described_class.encode(user_id: 42)
+        expect(token).not_to eq(token_str)
+      end
+    end
+  end
+
+  describe '.decode' do
+    it 'generates token' do
+      Timecop.freeze(Time.local(2017, 2, 26)) do
+        token = described_class.decode(token_str)
+        expect(token).to eq('user_id' => 42, 'exp' => 1_488_164_400)
+      end
+    end
+
+    it 'is nil when exception' do
+      Timecop.freeze(Time.local(2017, 2, 26)) do
+        token = described_class.decode(nil)
+        expect(token).to be_nil
+      end
+    end
+  end
+end
