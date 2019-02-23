49aab2fefddc3315054fd11c2e6f6e0f54a9f21d
Vagner Zampieri <vagner.zampieri@myfreecomm.com.br>
2019-02-23 14:12:52 -0300

    Remove offenses

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -6,11 +6,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, secret_key_base, 'HS256')
+      JWT.encode(payload, secret_key_base, "HS256")
     end
 
     def decode(token)
-      body = JWT.decode(token, secret_key_base, true, algorithm: 'HS256')[0]
+      body = JWT.decode(token, secret_key_base, true, algorithm: "HS256")[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
