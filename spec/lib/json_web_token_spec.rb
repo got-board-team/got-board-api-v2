745ee9ff389e3c3cc94fc74ebe03a47d43f14db9
Vagner Zampieri <vagner.zampieri@myfreecomm.com.br>
2019-02-24 10:04:08 -0300

    Change JsonWebToken test

--- spec/lib/json_web_token_spec.rb
+++ spec/lib/json_web_token_spec.rb
@@ -7,38 +7,33 @@ RSpec.describe JsonWebToken, type: :lib do
   end
 
   before do
+    Timecop.freeze(Time.local(2017, 2, 26))
     allow(Rails.application.secrets).to receive(:secret_key_base).and_return("FOO")
   end
 
   describe '.encode' do
+    subject { described_class.encode(user_id: 42) }
+
     it 'generates token' do
-      Timecop.freeze(Time.local(2017, 2, 26)) do
-        token = described_class.encode(user_id: 42)
-        expect(token).to eq(token_str)
-      end
+      expect(subject).to eq(token_str)
     end
 
     it 'generates other token' do
       Timecop.freeze(Time.local(2017, 2, 27)) do
-        token = described_class.encode(user_id: 42)
-        expect(token).not_to eq(token_str)
+        expect(subject).not_to eq(token_str)
       end
     end
   end
 
   describe '.decode' do
     it 'generates token' do
-      Timecop.freeze(Time.local(2017, 2, 26)) do
-        token = described_class.decode(token_str)
-        expect(token).to eq('user_id' => 42, 'exp' => 1_488_164_400)
-      end
+      token = described_class.decode(token_str)
+      expect(token).to eq('user_id' => 42, 'exp' => 1_488_164_400)
     end
 
     it 'is nil when exception' do
-      Timecop.freeze(Time.local(2017, 2, 26)) do
-        token = described_class.decode(nil)
-        expect(token).to be_nil
-      end
+      token = described_class.decode(nil)
+      expect(token).to be_nil
     end
   end
 end
