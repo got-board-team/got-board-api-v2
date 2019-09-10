9969c6de4b802a94ce283c7da11c7d713f47c3fc
Rafael B. Tauil <rafael@tauil.com.br>
2019-09-10 08:18:26 +0200

    WIP

--- app/controllers/combat_controller.rb
+++ app/controllers/combat_controller.rb
@@ -2,13 +2,20 @@ class CombatController < ApplicationController
   include CurrentGame
 
   def update
-    Pusher.trigger("game", "combat", id: params[:id], type: "combat", attributes: update_params)
-    render json: update_params
+    output_json = update_params
+    output_json[:game_id] = params[:data][:game_id]
+    output_json[:house_name] = params[:data][:house_name]
+
+    Pusher.trigger("game", "combat", game_id: output_json[:game_id], type: "combat", house_name: output_json[:house_name], attributes: update_params)
+
+    puts "\n\n\n#{params}\n\n#{update_params}\n\n#{output_json}\n\n\n"
+    render json: output_json
   end
 
   def update_params
     params.require(:data).require(:attributes).permit(
       :started,
+      :reset,
       :revealed,
       choosenCard: [:id, :name, :houseName]
     )
